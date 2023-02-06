9753411aaafee84c8214ac4d9180774a1c44e30f
Isaac Vallhonrat <isaac.vallhonrat@dynatrace.com>
2023-02-06 11:31:14 +0100

    fix code formatting

--- src/log/processing/log_processing_rule.py
+++ src/log/processing/log_processing_rule.py
@@ -24,6 +24,7 @@ from utils.helpers import helper_regexes
 
 logger = logging.getLogger(__name__)
 
+
 @dataclass(frozen=True)
 class LogProcessingRule:
     name: str
@@ -53,64 +54,73 @@ class LogProcessingRule:
 
         # Validate mandatory strings
         for i in [self.name, self.source, self.log_format, self.known_key_path_pattern]:
-            if not isinstance(i,str):
+            if not isinstance(i, str):
                 raise ValueError(f"{i} is not a string.")
 
         # validate optional strings
         for i in [self.attribute_extraction_grok_expression,
-                  self.log_entries_key,self.filter_json_objects_key,
+                  self.log_entries_key, self.filter_json_objects_key,
                   self.filter_json_objects_value]:
-            if not (isinstance(i,str) or i is None):
+            if not (isinstance(i, str) or i is None):
                 raise ValueError(f"{i} is not a string.")
 
         # validate optional dicts
         for i in [self.annotations, self.attribute_extraction_from_key_name,
                   self.attribute_extraction_jmespath_expression,
                   self.attribute_extraction_from_top_level_json]:
-            if not (isinstance(i,dict) or i is None):
+            if not (isinstance(i, dict) or i is None):
                 raise ValueError(f"{i} is not a dict.")
-        
+
         # validate attribute extraction from top level json
         if (self.attribute_extraction_from_top_level_json and self.log_format != "json_stream" and
            self.log_entries_key is None):
-            raise ValueError("attribute_extraction_from_top_level_json is only valid for JSON Stream with JSON list entries")
-        
-        #validate filter json key-value if not None
+            raise ValueError(
+                "attribute_extraction_from_top_level_json is only valid for JSON Stream with JSON list entries")
+
+        # validate filter json key-value if not None
         if self.filter_json_objects_key:
             if not self.filter_json_objects_value:
-                raise ValueError("filter_json_objects_value can't be None if filter_json_objects_key is specified")
+                raise ValueError(
+                    "filter_json_objects_value can't be None if filter_json_objects_key is specified")
 
         # validate optional lists
-        if not (isinstance(self.requester,list) or self.requester is None):
+        if not (isinstance(self.requester, list) or self.requester is None):
             raise ValueError("requester is not a list.")
-        elif isinstance(self.requester,list):
+        elif isinstance(self.requester, list):
             for i in self.requester:
-                if not isinstance(i,str):
-                    raise ValueError("requester list contains non-string items")
+                if not isinstance(i, str):
+                    raise ValueError(
+                        "requester list contains non-string items")
 
         # validate log format
         if self.log_format not in ['text', 'json', 'json_stream']:
-            raise ValueError("log_format must be either text, json or json_stream.")
+            raise ValueError(
+                "log_format must be either text, json or json_stream.")
 
     def __post_init__(self):
         self.validate()
         # Compile Regular expression here using defined helper patterns
-        object.__setattr__(self,"known_key_path_pattern_regex",re.compile(self.known_key_path_pattern.format(**helper_regexes)))
+        object.__setattr__(self, "known_key_path_pattern_regex", re.compile(
+            self.known_key_path_pattern.format(**helper_regexes)))
 
         if self.attribute_extraction_from_key_name is not None:
             # Compile regular expressions for attribute extraction from key using the defined helper patterns
             compiled_regexes_dict = {}
-            for k,v in self.attribute_extraction_from_key_name.items():
-                compiled_regexes_dict[k] = re.compile(v.format(**helper_regexes))
-            object.__setattr__(self,"attribute_extraction_from_key_name_regex",compiled_regexes_dict)
+            for k, v in self.attribute_extraction_from_key_name.items():
+                compiled_regexes_dict[k] = re.compile(
+                    v.format(**helper_regexes))
+            object.__setattr__(
+                self, "attribute_extraction_from_key_name_regex", compiled_regexes_dict)
         else:
-            object.__setattr__(self,"attribute_extraction_from_key_name_regex",None)
-        
+            object.__setattr__(
+                self, "attribute_extraction_from_key_name_regex", None)
+
         # Load Grok object once. PyGrok reloads all patterns on __init__
         if self.attribute_extraction_grok_expression is not None:
-            object.__setattr__(self,"attribute_extraction_grok_object",Grok(self.attribute_extraction_grok_expression))
+            object.__setattr__(self, "attribute_extraction_grok_object", Grok(
+                self.attribute_extraction_grok_expression))
         else:
-            object.__setattr__(self,"attribute_extraction_grok_object",None)
+            object.__setattr__(self, "attribute_extraction_grok_object", None)
 
     def get_attributes_from_s3_key_name(self, key: str):
         '''
@@ -124,7 +134,7 @@ class LogProcessingRule:
                     injected_attributes.update({dt_attribute: attrib.group()})
         return injected_attributes
 
-    def get_extracted_log_attributes(self,message) -> dict:
+    def get_extracted_log_attributes(self, message) -> dict:
         '''
         Receives the log message (dict or str) and extracts attributes.
         Text log: apply grok expression if it exists; then apply jmespath expression if it exists to calculate additional fields.
@@ -135,19 +145,23 @@ class LogProcessingRule:
         json_message = {}
 
         if self.attribute_extraction_grok_object is not None:
-            if isinstance(message,str):
-                grok_attributes = self.attribute_extraction_grok_object.match(message)
+            if isinstance(message, str):
+                grok_attributes = self.attribute_extraction_grok_object.match(
+                    message)
                 if grok_attributes is not None:
                     attributes_dict.update(grok_attributes)
                     # Create JSON message, in case we need to apply also JMESPATH to new extracted attributes
                     json_message.update(grok_attributes)
                 else:
-                    logger.warning(f'Grok expression did not match log message: {message} --> No attributes extracted.')
+                    logger.warning(
+                        'Grok expression did not match log message: %s --> No attributes extracted.',
+                        message)
             else:
-                logger.warning("Can't apply Grok expression to non-str object.")
-        
+                logger.warning(
+                    "Can't apply Grok expression to non-str object.")
+
         # if no grok expression, check if message is a Dict
-        if isinstance(message,dict):
+        if isinstance(message, dict):
             json_message = message
 
         if self.attribute_extraction_jmespath_expression is not None:
@@ -166,15 +180,15 @@ class LogProcessingRule:
             attributes.update(self.annotations)
 
         return attributes
-    
-    def get_all_attributes(self,message,s3_key_name):
+
+    def get_all_attributes(self, message, s3_key_name):
         '''
         Returns all attributes generated for a given log message (str or dict) and S3 key:
           - Attributes extracted from S3 Key Name
           - Attributes extracted with Grok and Jmespath expressions
           - LogProcessingRule specific annotations
         '''
-        
+
         attributes = {}
 
         attributes.update(self.get_attributes_from_s3_key_name(s3_key_name))
@@ -183,7 +197,7 @@ class LogProcessingRule:
 
         return attributes
 
-    def match_s3_key(self,key_name:str) -> bool:
+    def match_s3_key(self, key_name: str) -> bool:
         if self.known_key_path_pattern_regex.match(key_name):
             return True
         else:
--- src/log/processing/log_processing_rules.py
+++ src/log/processing/log_processing_rules.py
@@ -30,6 +30,7 @@ AVAILABLE_LOG_SOURCES = ['aws', 'generic', 'custom']
 
 logger = logging.getLogger()
 
+
 def list_rules_in_dir(rules_dir):
     '''
     List yaml files within directory and subdirectories
@@ -40,7 +41,7 @@ def list_rules_in_dir(rules_dir):
     for path, dirs, files in os.walk(rules_dir):
         for file in files:
             if is_yaml_file(file):
-                log_processing_rule_files.append(os.path.join(path,file))
+                log_processing_rule_files.append(os.path.join(path, file))
 
     return log_processing_rule_files
 
@@ -52,50 +53,56 @@ def create_log_processing_rule(rule_dict):
 
     # Validate we have all required attributes, and set to None the ones undefined
 
-    required_attributes = ['name', 'source','known_key_path_pattern', 'log_format']
-    optional_attributes = ['log_entries_key', 'annotations','requester',
+    required_attributes = ['name', 'source',
+                           'known_key_path_pattern', 'log_format']
+    optional_attributes = ['log_entries_key', 'annotations', 'requester',
                            'attribute_extraction_from_key_name', 'attribute_extraction_grok_expression',
                            'attribute_extraction_jmespath_expression', 'filter_json_objects_key',
-                           'filter_json_objects_value','attribute_extraction_from_top_level_json']
+                           'filter_json_objects_value', 'attribute_extraction_from_top_level_json']
 
     for attribute in required_attributes:
         if attribute not in rule_dict.keys():
             raise InvalidLogProcessingRuleFile
-    
+
     for attribute in optional_attributes:
         if attribute not in rule_dict.keys():
             rule_dict[attribute] = None
-    
+
     # Check that source on the rule is valid. (
     if rule_dict['source'] not in AVAILABLE_LOG_SOURCES:
-       raise InvalidLogProcessingRuleFile(message="Invalid log source on processing rule.")
+        raise InvalidLogProcessingRuleFile(
+            message="Invalid log source on processing rule.")
 
     try:
         processing_rule = LogProcessingRule(
-            name= rule_dict['name'],
-            source = rule_dict['source'],
-            known_key_path_pattern = rule_dict['known_key_path_pattern'],
-            log_format = rule_dict['log_format'],
-            filter_json_objects_key = rule_dict['filter_json_objects_key'],
-            filter_json_objects_value = rule_dict['filter_json_objects_value'],
-            log_entries_key = rule_dict['log_entries_key'],
-            annotations = rule_dict['annotations'],
-            requester = rule_dict['requester'],
-            attribute_extraction_from_key_name = rule_dict['attribute_extraction_from_key_name'],
-            attribute_extraction_grok_expression = rule_dict['attribute_extraction_grok_expression'],
-            attribute_extraction_jmespath_expression = rule_dict['attribute_extraction_jmespath_expression'],
-            attribute_extraction_from_top_level_json = rule_dict['attribute_extraction_from_top_level_json']
+            name=rule_dict['name'],
+            source=rule_dict['source'],
+            known_key_path_pattern=rule_dict['known_key_path_pattern'],
+            log_format=rule_dict['log_format'],
+            filter_json_objects_key=rule_dict['filter_json_objects_key'],
+            filter_json_objects_value=rule_dict['filter_json_objects_value'],
+            log_entries_key=rule_dict['log_entries_key'],
+            annotations=rule_dict['annotations'],
+            requester=rule_dict['requester'],
+            attribute_extraction_from_key_name=rule_dict['attribute_extraction_from_key_name'],
+            attribute_extraction_grok_expression=rule_dict['attribute_extraction_grok_expression'],
+            attribute_extraction_jmespath_expression=rule_dict[
+                'attribute_extraction_jmespath_expression'],
+            attribute_extraction_from_top_level_json=rule_dict[
+                'attribute_extraction_from_top_level_json']
         )
     except ValueError as ex:
-        raise InvalidLogProcessingRuleFile("Error parsing log processing rule.") from ex
+        raise InvalidLogProcessingRuleFile(
+            "Error parsing log processing rule.") from ex
 
     return processing_rule
+
 
 def load_rules_from_dir(directory: str) -> dict:
     '''
     Looks for yaml files on the given directory and its subdirectories and attempts to load them as LogProcessingRule(s)
     '''
-    
+
     log_processing_rule_files = list_rules_in_dir(directory)
 
     # initialize log_processing_rules
@@ -112,12 +119,15 @@ def load_rules_from_dir(directory: str) -> dict:
                 if not isinstance(processing_rule_dict, dict):
                     raise InvalidLogProcessingRuleFile(file=rule_file)
 
-                log_processing_rules[processing_rule_dict['source']][processing_rule_dict['name']] = create_log_processing_rule(processing_rule_dict)
+                log_processing_rules[processing_rule_dict['source']][processing_rule_dict['name']
+                                                                     ] = create_log_processing_rule(processing_rule_dict)
 
         except (KeyError, InvalidLogProcessingRuleFile):
-            logger.exception("There was an error creating the log processing rule. Log processing rule is invalid.")
-    
+            logger.exception(
+                "There was an error creating the log processing rule. Log processing rule is invalid.")
+
     return log_processing_rules
+
 
 def load_processing_rules_from_yaml(body: str):
     '''
@@ -140,19 +150,24 @@ def load_processing_rules_from_yaml(body: str):
         yaml_iterator = yaml.load_all(body, Loader=yaml.SafeLoader)
         for i, processing_rule_dict in enumerate(yaml_iterator):
             try:
-                if isinstance(processing_rule_dict,dict):
-                    log_processing_rules[processing_rule_dict['source']][processing_rule_dict['name']] = create_log_processing_rule(processing_rule_dict)
+                if isinstance(processing_rule_dict, dict):
+                    log_processing_rules[processing_rule_dict['source']][processing_rule_dict['name']
+                                                                         ] = create_log_processing_rule(processing_rule_dict)
                 elif processing_rule_dict is None:
                     logger.warning("Skipping empty log processing rule")
                 else:
-                    raise InvalidLogProcessingRuleFile(file=str(i), message="Invalid log forwarding rule file")
+                    raise InvalidLogProcessingRuleFile(
+                        file=str(i), message="Invalid log forwarding rule file")
             except (KeyError, InvalidLogProcessingRuleFile):
-                logger.exception("There was an error creating the log processing rule. Log processing rule %s is invalid", str(i))
-        
+                logger.exception(
+                    "There was an error creating the log processing rule. Log processing rule %s is invalid", str(i))
+
     except yaml.YAMLError:
-        logger.exception("Encountered an error while parsing log-processing-rules. Aborting...")
-    
+        logger.exception(
+            "Encountered an error while parsing log-processing-rules. Aborting...")
+
     return log_processing_rules
+
 
 def load_custom_rules_from_aws_appconfig():
     '''
@@ -162,11 +177,14 @@ def load_custom_rules_from_aws_appconfig():
 
     logger.info("Loading custom log-processing-rules from AWS AppConfig...")
 
-    raw_processing_rules = aws_appconfig_helpers.get_configuration_from_aws_appconfig('log-processing-rules')
+    raw_processing_rules = aws_appconfig_helpers.get_configuration_from_aws_appconfig(
+        'log-processing-rules')
 
-    log_processing_rules = load_processing_rules_from_yaml(raw_processing_rules['Body'])
+    log_processing_rules = load_processing_rules_from_yaml(
+        raw_processing_rules['Body'])
 
     return log_processing_rules, raw_processing_rules['Configuration-Version']
+
 
 def load_custom_rules_from_local_file():
     '''
@@ -174,17 +192,20 @@ def load_custom_rules_from_local_file():
     containing a dict with the rules and the Configuration-Version number.
     '''
 
-    logger.info("Loading custom log-processing-rules from local file config/log-processing-rules.yaml ...")
+    logger.info(
+        "Loading custom log-processing-rules from local file config/log-processing-rules.yaml ...")
 
     try:
-        with open(file=DEFAULT_CUSTOM_LOG_PROCESSING_RULES_FILE,mode='r',encoding=ENCODING) as file:
+        with open(file=DEFAULT_CUSTOM_LOG_PROCESSING_RULES_FILE, mode='r', encoding=ENCODING) as file:
             log_processing_rules = load_processing_rules_from_yaml(file.read())
     except OSError:
-        logger.exception("Unable to open local file %s", DEFAULT_CUSTOM_LOG_PROCESSING_RULES_FILE)
+        logger.exception("Unable to open local file %s",
+                         DEFAULT_CUSTOM_LOG_PROCESSING_RULES_FILE)
         raise
 
     return log_processing_rules
-     
+
+
 class InvalidLogProcessingRuleFile(Exception):
 
     def __init__(self, file=None, message='Processing rule file is invalid.'):
@@ -194,13 +215,13 @@ class InvalidLogProcessingRuleFile(Exception):
     def __str__(self):
         return f"{self.message} --> {self.file}"
 
-    
+
 def load_built_in_rules():
     '''
     Load built-in log processing rules. 
     '''
     return load_rules_from_dir(BUILT_IN_PROCESSING_RULES_PATH)
-    
+
 
 def load_custom_rules():
     '''
@@ -217,11 +238,12 @@ def load_custom_rules():
             log_processing_rules_directory = os.environ['LOG_PROCESSING_RULES_PATH']
         else:
             log_processing_rules_directory = DEFAULT_CUSTOM_LOG_PROCESSING_RULES_PATH
-        
+
         if os.path.isfile(DEFAULT_CUSTOM_LOG_PROCESSING_RULES_FILE):
             log_processing_rules = load_custom_rules_from_local_file()
         elif os.path.isdir(log_processing_rules_directory):
-            loaded_log_processing_rules = load_rules_from_dir(log_processing_rules_directory)
+            loaded_log_processing_rules = load_rules_from_dir(
+                log_processing_rules_directory)
             # if not an empty dict
             if loaded_log_processing_rules:
                 log_processing_rules = loaded_log_processing_rules
@@ -232,6 +254,7 @@ def load_custom_rules():
                     log_processing_rules[log_source] = {}
 
     return log_processing_rules, log_processing_rules_verison
+
 
 def load():
     '''
@@ -246,15 +269,16 @@ def load():
         log_processing_rules[log_source] = {}
 
     # iterate over built-in and custom rules content to generate combined rules dict
-    for source,built_in_rule_dict in built_in_rules.items():
+    for source, built_in_rule_dict in built_in_rules.items():
         log_processing_rules[source].update(built_in_rule_dict)
 
-    for custom_source,custom_rule_dict in custom_rules.items():
+    for custom_source, custom_rule_dict in custom_rules.items():
         log_processing_rules[custom_source].update(custom_rule_dict)
 
     return log_processing_rules, custom_rules_version
 
-def lookup_processing_rule(source: str , source_name: str, processing_rules: dict, key_name: str):
+
+def lookup_processing_rule(source: str, source_name: str, processing_rules: dict, key_name: str):
     '''
     Given a dict of processing rules:
         {
@@ -270,15 +294,15 @@ def lookup_processing_rule(source: str , source_name: str, processing_rules: dic
 
     if not source in AVAILABLE_LOG_SOURCES or source is None:
         return None
-    # is it a generic or custom rule? 
+    # is it a generic or custom rule?
     elif source == 'custom' or source == 'generic':
         try:
             logger.debug('Matched log processing rule %s.%s',
-            source,source_name)
+                         source, source_name)
             return processing_rules[source][source_name]
         except KeyError:
             logger.warning("No matching log processing rule for %s.%s. "
-                            "Defaulting to 'generic' log ingestion.", source, source_name)
+                           "Defaulting to 'generic' log ingestion.", source, source_name)
             return processing_rules['generic']['generic']
     elif source == 'aws':
         matched_processing_rule = None
@@ -288,5 +312,6 @@ def lookup_processing_rule(source: str , source_name: str, processing_rules: dic
                 logger.debug("Matched aws log processing rule %s", name)
                 return processing_rule
         if not matched_processing_rule:
-            logger.warning("Couldn't find a matching aws processing rule for %s. Defaulting to generic ingestion.", key_name)
+            logger.warning(
+                "Couldn't find a matching aws processing rule for %s. Defaulting to generic ingestion.", key_name)
             return processing_rules['generic']['generic']
