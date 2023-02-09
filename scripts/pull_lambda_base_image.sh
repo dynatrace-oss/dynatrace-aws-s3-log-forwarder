ebaeafe841e556e52caa9324a63972b0c2f3cbbf
Isaac Vallhonrat <isaac.vallhonrat@dynatrace.com>
2023-02-09 20:11:03 +0100

    remove unnecessary script

--- scripts/pull_lambda_base_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-# Build dev image with dev requirements
-docker pull public.ecr.aws/lambda/python:${LAMBDA_BASE_IMAGE_TAG}
-export BASE_CONTAINER_IMAGE_SHA256=$(docker inspect --format='{{index .RepoDigests 0}}' public.ecr.aws/lambda/python:${LAMBDA_BASE_IMAGE_TAG})
-echo "Building Lambda container image from tag ${LAMBDA_BASE_IMAGE_TAG} --> ${BASE_CONTAINER_IMAGE_SHA256}"
