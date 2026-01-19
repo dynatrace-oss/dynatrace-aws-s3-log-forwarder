bc7ca7b33ba142470fdc3f34356c716ba1bdce92
sq2gxo <marek.garwolinski@dynatrace.com>
2026-01-19 12:51:16 +0100

    use consistent spacing

--- tests/e2e/local_app.py
+++ tests/e2e/local_app.py
@@ -113,7 +113,7 @@ def load_s3_test_data_from_disk():
                         logger.debug(f'saving data mock s3 {bucket}/{key}')
                         s3 = boto3.client('s3')
                         s3.create_bucket(Bucket=bucket)
-                        s3.put_object(Bucket=bucket, Key=key, Body=data, ExpectedBucketOwner = account_id)
+                        s3.put_object(Bucket=bucket, Key=key, Body=data, ExpectedBucketOwner=account_id)
 
                         event_records.append('{"messageId": "' + hashlib.md5(key.encode('utf-8')).hexdigest() +
                                         '","body": "{\\"region\\":\\"us-east-1\\",\\"detail\\":{\\"bucket\\":{\\"name\\":\\"' + bucket + '\\"},\\"object\\":{\\"key\\":\\"' + key + '\\"},\\"requester\\":\\"' + requester + '\\"}}"}')
